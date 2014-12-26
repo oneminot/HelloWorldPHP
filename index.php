9a93cc05c77b355cb5bb9510465f420a87ea3b33
kushal <kushaldeveloper@gmail.com>
2014-12-25 20:10:08 -0600

    angular js

--- index.php
+++ index.php
@@ -5,7 +5,9 @@
         <title>Hello, world!</title>
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
-        <script src="js/bootstrap.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>        
+        <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.28/angular.min.js"></script>
+
     </head>
     <body>
         <?php 
