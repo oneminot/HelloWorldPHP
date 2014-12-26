34d73698fceab7056441e846e2fb039e981f54c8
Student <Student@165.234.211.150>
2014-12-25 19:44:54 -0600

    say hello, world

--- index.php
+++ index.php
@@ -7,11 +7,11 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Hello, world!</title>
     </head>
     <body>
         <?php
-        // put your code here
+            echo 'Hello, world!'
         ?>
     </body>
 </html>
