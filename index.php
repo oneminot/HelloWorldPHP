8482443051d97daeee1ccecfe248d4515aae8d35
kushal <kushaldeveloper@gmail.com>
2014-12-25 20:08:10 -0600

    a brave new world with bootstrap

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
     <head>
         <meta charset="UTF-8">
         <title>Hello, world!</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+        <script src="js/bootstrap.min.js"></script>
     </head>
     <body>
-        <?php
-            echo 'Hello, world!'
+        <?php 
+            echo 'Hello, world!';
         ?>
     </body>
 </html>
